23a40b4a374a39f27d04ac347553f5f6909aad17
seudad <seudad@icloud.com>
2026-05-31 18:15:32 +0300

    update

--- install.sh
+++ install.sh
@@ -19,8 +19,8 @@ read_input_yn() {
     echo
 }
 
-REPO_URL="https://github.com/fakelook1337/3xui-shopbot.git"
-PROJECT_DIR="3xui-shopbot"
+REPO_URL="https://github.com/seudad/shop_vpn_bot.git"
+PROJECT_DIR="shop_vpn_bot"
 NGINX_CONF_FILE="/etc/nginx/sites-available/${PROJECT_DIR}.conf"
 
 echo -e "${GREEN}--- Запуск скрипта установки/обновления 3xui-ShopBot ---${NC}"
